8849cb91c45920bcd0860513ba91c55646407ffc
Jared Sorge <jared@jsorge.net>
2016-02-07 17:40:00 -0800

    Model implementation

--- nesting tables/Model.swift
+++ nesting tables/Model.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+enum Image: String {
+    case Male = "male"
+    case Female = "female"
+}
+
 
 struct Company {
     let name: String
@@ -16,6 +21,34 @@ struct Company {
 
 struct Worker {
     let name: String
-    let imageName: String
+    let imageName: Image
 }
 
+
+func generateCompanies() -> [Company] {
+    var companies = [Company]()
+    
+    do {
+        let jared = Worker(name: "Jared", imageName: .Male)
+        let adam = Worker(name: "Adam", imageName: .Male)
+        let molly = Worker(name: "Molly", imageName: .Female)
+        let zulily = Company(name: "zulily", workers: [jared, adam, molly])
+        companies.append(zulily)
+    }
+    
+    do {
+        let steve = Worker(name: "Steve", imageName: .Male)
+        let tim = Worker(name: "Tim", imageName: .Male)
+        let angela = Worker(name: "Angela", imageName: .Female)
+        let apple = Company(name: "Apple", workers: [steve, tim, angela])
+        companies.append(apple)
+    }
+    
+    do {
+        let marissa = Worker(name: "Marissa", imageName: .Female)
+        let yahoo = Company(name: "Yahoo", workers: [marissa])
+        companies.append(yahoo)
+    }
+    
+    return companies
+}
